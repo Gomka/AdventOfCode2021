483f111dbaa581e4befc17b7c7b0268ef6256b03
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2021-12-06 20:20:36 +0100

    Day 1 completed

--- src/Challenges/Challenge1.java
+++ src/Challenges/Challenge1.java
@@ -1,18 +1,23 @@
 package Challenges;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Stack;
 
 public class Challenge1 implements IChallenge {
 
     @Override
     public String runChallenge() {
 
+        return ("Challenge 1: \n"+
+                "   Part one: " + partOne() +"\n" +
+                "   Part two: " + partTwo());
+    }
+
+    public int partOne() {
         int numIncrements = 0, previousDepth = -1;
         boolean firstLine = true;
-
 
         try (BufferedReader br = new BufferedReader(new FileReader("data/Challenge1Input.txt"))) {
             String line;
@@ -28,12 +33,41 @@ public class Challenge1 implements IChallenge {
                     previousDepth = Integer.parseInt(line);
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return ("Challenge 1: " + numIncrements);
+        return numIncrements;
+    }
+
+    public int partTwo() {
+        int numIncrements = 0, previousDepth = -1;
+        Stack<Integer> st = new Stack<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data/Challenge1Input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                st.push(Integer.parseInt(line));
+
+                if(st.size() >= 3) {
+                    if(previousDepth < 0) {
+                        previousDepth = st.get(0) + st.get(1) + st.get(2);
+                    } else {
+                        if (previousDepth < st.get(0) + st.get(1) + st.get(2)) {
+                            numIncrements++;
+                        }
+                        previousDepth = st.get(0) + st.get(1) + st.get(2);
+                    }
+
+                    st.remove(0);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return numIncrements;
     }
 }
