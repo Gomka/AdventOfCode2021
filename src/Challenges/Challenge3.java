29bcab05d9dccad89cc97f9ecbb2ef2290d8c45e
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2021-12-06 23:08:03 +0100

    Day three completed

--- src/Challenges/Challenge3.java
+++ src/Challenges/Challenge3.java
@@ -3,6 +3,8 @@ package Challenges;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Challenge3 implements IChallenge {
 
@@ -15,7 +17,7 @@ public class Challenge3 implements IChallenge {
     }
 
     public int partOne() {
-        String gamma = "", epsilon = "1";
+        String gamma = "", epsilon = "";
         int[][] digits = new int[12][2];
 
         try (BufferedReader br = new BufferedReader(new FileReader("data/Challenge3Input.txt"))) {
@@ -51,6 +53,66 @@ public class Challenge3 implements IChallenge {
     }
 
     public int partTwo() {
-        return 0;
+        String oxygenRating = "0", co2Rating = "0";
+        List<String> startsWithOne = new LinkedList<String>();
+        List<String> startsWithZero = new LinkedList<String>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data/Challenge3Input.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                
+                if(line.charAt(0) == '0') {
+                    startsWithZero.add(line);
+                } else {
+                    startsWithOne.add(line);
+                }
+                
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (startsWithOne.size() >= startsWithZero.size()) {
+            oxygenRating = findRating(startsWithOne, true);
+            co2Rating = findRating(startsWithZero, false);
+        } else {
+            oxygenRating = findRating(startsWithZero, true);
+            co2Rating = findRating(startsWithOne, false);
+        }
+
+        return Integer.parseInt(oxygenRating, 2) * Integer.parseInt(co2Rating, 2);
+    }
+
+    public String findRating(List<String> values, boolean oxygen) {
+
+        for (int i = 1; i < 12; i++) {
+            int countOnes = 0;
+            int countZeroes = 0;
+
+            for (String str:values) {
+                if (str.charAt(i) == '1') {
+                    countOnes++;
+                } else countZeroes++;
+            }
+
+            List<String> stringsToRemove = new LinkedList<>();
+            for (String str:values) {
+                if (str.charAt(i) == '0' && ((countOnes >= countZeroes && oxygen) || (countOnes < countZeroes && !oxygen))) {
+                    stringsToRemove.add(str);
+                } else if (str.charAt(i) == '1' && ((countOnes < countZeroes && oxygen) || (countOnes >= countZeroes && !oxygen))){
+                    stringsToRemove.add(str);
+                }
+            }
+
+            for (String str:stringsToRemove) {
+                values.remove(str);
+            }
+
+            if (values.size() == 1) break;
+
+        }
+
+        return values.get(0);
     }
 }
